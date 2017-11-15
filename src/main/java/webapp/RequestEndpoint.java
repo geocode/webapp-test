e27afafd5e4c60643e12f8206db1718854e23950
Geoff Ruscoe <Geoff_Ruscoe@papajohns.com>
2017-11-15 14:09:48 -0500

    Ready to push new version

--- src/main/java/webapp/RequestEndpoint.java
+++ src/main/java/webapp/RequestEndpoint.java
@@ -14,7 +14,7 @@ public class RequestEndpoint {
     @Produces(MediaType.TEXT_PLAIN)
     public String submit() {
 
-	String output = "New Production Version -- v2 -- request received and sent to event log";
+	String output = "New Production Version -- v3 -- request received and sent to event log";
         System.out.println(output);
         return output;
     }
