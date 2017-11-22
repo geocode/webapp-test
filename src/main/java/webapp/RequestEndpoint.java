5362d17b888fcf3cf7a07f818f2b2ca770adb293
Geoff Ruscoe <Geoff_Ruscoe@papajohns.com>
2017-11-22 09:01:21 -0500

    Push new version to prod

--- src/main/java/webapp/RequestEndpoint.java
+++ src/main/java/webapp/RequestEndpoint.java
@@ -14,7 +14,7 @@ public class RequestEndpoint {
     @Produces(MediaType.TEXT_PLAIN)
     public String submit() {
 
-	String output = "New Production Version -- v8.34.5.0 -- request received and sent to event log";
+	String output = "Original Production Version -- v8.34.5.0 -- restful service call received and new event has been sent to event log";
         System.out.println(output);
         return output;
     }
